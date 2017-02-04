a76121977c5fe5eba002a803c60b34a8f9274a61
Twinkling <twinkling8023@gmail.com>
2017-02-04 16:51:22 +0800

    improve JQSelect
    完善JQSelect插件
    
    Signed-off-by: Twinkling <twinkling8023@gmail.com>

--- JQSelect/js/JQSelect.js
+++ JQSelect/js/JQSelect.js
@@ -3,23 +3,15 @@
 		/* properties */
 		width: 100,
 		height: 30,
+		name: '',
 		textAlign: 'center',
 		valueField: 'value',
 		textField: 'text',
-		url: '',
+		url: null,
 		method: 'get',
 		queryParams: {},
-		data: [],
-		filter: function() {},  // filter data
-
-		/* method */
-		/*options: function() {},  // get instance options
-		getData: function() {},  // get loaded list data
-		loadData: function(data) {},  // load locale list data
-		reload: function(url) {},  // request romote data,pass parameter url override original url value
-		setValue: function(value) {},  // set value
-		getValue: function() {},  // get value
-		clear: function() {},  // clear select value*/
+		data: null,  // local data, override remote data
+		filter: function(data) {return data;},  // filter data
 
 		/* events */
 		onChange: function(newVal, oldVal) {},  // fires when change selected data
@@ -35,64 +27,235 @@
 		;
 	}
 
-	/* parser data(include local & remote & DOM) */
-	function parserData(data) {
-		if(typeof data == 'Array') {
-			;
-		} else {
-			;
-		}
-		function _parserArray() {
-			
+	/* parser DOM struct, parser options */
+	function parserDOM(DOM) {
+		;
+	}
+
+	function setValues(target, value) {
+		var state = $.data(target, 'JQSelect');
+		var opts = state.options;
+		var data = state.data;
+		var $JQSelect = state.JQSelect;
+		var oldVal = $(target).JQSelect('getValue');
+		if(opts.onBeforeSelect.call(target, oldVal) == false) {return ;}
+		if(typeof value == 'string') {
+			var selected = data.filter(function(item, index, arr) {
+				return item.value === value;
+			});
+			$JQSelect.find('.JQSelect-selected input').val(selected[0].value);
+			$JQSelect.find('.val').text(selected[0].text);
+			opts.onChange.call(target, oldVal, value);
+			opts.onAfterSelect.call(target, value);
 		}
 	}
+
+	function render(target) {
+		var state = $.data(target, 'JQSelect');
+		var $JQSelect = state.JQSelect;
+		var opts = state.options;
+		var data = state.data;
+		var value, text, option, options = [];
+
+		for(var i = 0;i < data.length; i ++) {
+			value = data[i][opts.valueField];
+			text = data[i][opts.textField];
+			option = '<li data-value="' + value + '"><span class="JQSelect-blockCenMid">' + text + '</span></li>';
+			options.push(option);
+		}
+
+		options = $(options.join(''));
+		$JQSelect.find('.JQSelect-options ul').empty().append(options);
+
+		for(var i = 0;i < options.length; i ++) {
+			options.eq(i).bind('click.JQSelect', {target: target}, _clickHandler);
+		}
+
+		function _clickHandler(e) {
+			value = $(this).attr('data-value');
+			$(target).JQSelect('setValue', value);
+
+			$JQSelect.find('.JQSelect-icon').removeClass('JQSelect-rotate180');
+			$JQSelect.find('.JQSelect-options').slideUp();
+
+			e.stopPropagation();
+		}
+	}
+
+	/* load data */
+	function loadData(target, data) {
+		var state = $.data(target, 'JQSelect');
+		var opts = state.options;
+		state.data = opts.filter.call(target, data);
+		
+		render(target);
+
+		opts.onLoadSuccess.call(target, data);
+	}
+
+    /* request remote data if url is setted */
+	function request(target, url, params) {
+		var opts = $.data(target, 'JQSelect').options;
+		if(url) {
+			opts.url = url;
+		}
+		params = $.extend({}, opts.queryParams, params || {});
+		if(opts.onBeforeLoad.call(target, params) == false) {return ;}
+
+		opts.loader.call(target, params, function(data) {
+			loadData(target, data);
+		}, function(){
+			opts.onLoadError.apply(this, arguments);
+		});
+	}
+
+	/**/
+	function clickHandler(e) {
+		$(this).find('.JQSelect-icon').addClass('JQSelect-rotate180');
+		$(this).find('.JQSelect-options').slideDown();
+
+		e.stopPropagation();
+	}
+
+	/**/
+	function mouseleaveHandler(e) {
+		$(this).find('.JQSelect-icon').removeClass('JQSelect-rotate180');
+		$(this).find('.JQSelect-options').slideUp();
+		e.stopPropagation();
+	}
+
+	function create(target) {
+		var state = $.data(target, 'JQSelect');
+		var opts = state.options;
+
+		var $wrapper = $('<div class="JQSelect-wrapper" onselectstart="return false;" unselectable="on"></div>');
+		// $wrapper.appnd(target);
+		// $(target).hide();
+
+		var $select = $('<div class="JQSelect-selected"><input type="hidden" name="' + opts.name + '"><span class="JQSelect-blockCenMid val">请选择</span><i class="JQSelect-icon JQSelect-blockCenMid"></i></div>');
+		$select.css({height: opts.height, width: opts.width});
+		$select.find('.val').css({'text-align': opts.textAlign});
+		$wrapper.append($select);
+
+		var $optionsWrapper = $('<div class="JQSelect-options"></div>');
+		$optionsWrapper.css({top: opts.height, width: opts.width});
+		var $options = $('<ul></ul>');
+		$optionsWrapper.append($options).appendTo($wrapper);
+
+		// $wrapper.after(target);
+		$(target).after($wrapper).hide().prop('disabled', true);
+
+		for(var event in opts.events) {
+			$wrapper.bind(event + '.JQSelect', {target: target}, opts.events[event]);
+		}
+
+		state.JQSelect = $wrapper;
+	}
+
 	/* modify default options */
 	/*function modifyDefault(opts) {
 		;
 	}*/
 
-	$.fn.JQSelect = function(opts) {
-		opts = opts || {};
+	$.fn.JQSelect = function(opts, params) {			
+		if(typeof opts == 'string') {
+			var method = $.fn.JQSelect.method[opts];
+			if(method) {
+				return method(this, params);
+			}
+			return ;
+		}
+		var _this = this;
+		var defaults = $.fn.JQSelect.defaults;
+		opts = $.extend({}, defaults, opts);
 		return this.each(function() {
-			console.log(this);
+			var state = $.data(this, 'JQSelect');
+			if(state) {
+				$.extend(state.options, opts);
+			} else {
+				state = $.data(this, 'JQSelect', {
+					options: opts,
+					JQSelect: null,
+					data: []
+				});
+			}
+			create(this);
+			if(state.options.data) {
+				loadData(this, state.options.data);
+			}
+
+			request(this);
 		});
 	}
 
 	$.fn.JQSelect.method = {
-		options: function(jq) {},  // get instance options
-		getData: function(jq) {},  // get loaded list data
-		loadData: function(jq, data) {},  // load locale list data
-		reload: function(jq, url) {},  // request romote data,pass parameter url override original url value
-		setValue: function(jq, value) {},  // set value
-		getValue: function(jq) {},  // get value
-		clear: function(jq) {},  // clear select value
+		options: function(jq) {  // get instance options
+			return $.data(jq[0], 'JQSelect').options;
+			/*var options = [];
+			jq.each(function() {
+				options.push($.data(this, 'JQSelect').options);
+			});
+			return options;*/
+		},
+		getData: function(jq) {  // get loaded list data
+			return $.data(jq[0], 'JQSelect').data;
+			/*var data = [];
+			jq.each(function() {
+				data.push($.data(this, 'JQSelect').data);
+			});
+			return data;*/
+		},
+		loadData: function(jq, data) {  // load locale list data
+			return jq.each(function() {
+				loadData(this, data);
+			});
+		},
+		reload: function(jq, url) {  // request romote data,pass parameter url override original url value
+			return jq.each(function() {
+				if(typeof url == 'string') {
+					request(this, url);
+				}
+			});
+		},
+		setValue: function(jq, value) {  // set value
+			return jq.each(function() {
+				setValues(this, value);
+			});
+		},
+		getValue: function(jq) {  // get value
+			var $JQSelect = $.data(jq[0], 'JQSelect').JQSelect;
+			return $JQSelect.find('.JQSelect-selected input').val();
+		},
+		clear: function(jq) {  // clear select value
+			return jq.each(function() {
+				var $JQSelect = $.data(this, 'JQSelect').JQSelect;
+				$JQSelect.find('.JQSelect-selected input').val('');
+				$JQSelect.find('.val').text('请选择');
+			});
+		}
 	}
 
-	$.fn.JQSelect.defaults = $.extend({}, defaultOptions);
-
-	/*$.fn.extend({
-		"JQSelect": function(opts){
-			return this;
-			;
+	$.fn.JQSelect.defaults = $.extend({}, defaultOptions, {
+		events: {
+			click: clickHandler,
+			mouseleave: mouseleaveHandler
 		}
-	});*/
+		,loader: function(params, success, error) {
+			var opts = $(this).JQSelect('options');
+			if(!opts.url) {return ;}
+			$.ajax({
+				type: opts.method,
+				url: opts.url,
+				data: params,
+				dataType: 'json',
+				success: function(data) {
+					success(data);
+				},
+				error: function() {
+					error.apply(this, arguments)
+				}
+			});
+		}
+	});
 
-	$('.JQSelect-selected').click(function() {
-		$(this).find('.JQSelect-icon').addClass('JQSelect-rotate180');
-		$(this).next('.JQSelect-options').slideDown();
-	});
-	$('.JQSelect-wrapper').mouseleave(function() {
-		$(this).find('.JQSelect-icon').removeClass('JQSelect-rotate180');
-		$(this).find('.JQSelect-options').slideUp();
-	});
-	$('.JQSelect-options li').click(function() {
-		/* TODO */
-		var $parent = $(this).parents('.JQSelect-options');
-		var $prev = $parent.prev('.JQSelect-selected');
-		var text = $(this).find('span').text();
-		$prev.attr('data-value', $(this).attr('data-value'))
-			.find('span').text(text)
-			.next('.JQSelect-icon').removeClass('JQSelect-rotate180');
-		$parent.slideUp();
-	});
 })(jQuery);
